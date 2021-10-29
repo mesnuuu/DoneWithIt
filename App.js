1e693f4e2a279c3c31e1382ba197cb4af5f32b16
mesnuuu <mesnuuu@gmail.com>
2021-10-29 09:02:13 +0500

    Fundamental Concepts

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="inverted" />
+
+      <Text> {StatusBar.currentHeight} </Text>
+
+      <Text numberOfLines={5} onPress={handlePress}>
+        {" "}
+        Hello React Native.{" "}
+      </Text>
+
+      <Image source={require("./assets/icon.png")} />
+
+      <TouchableOpacity>
+        <Image
+          source={{ uri: "https://reactjs.org/logo-og.png" }}
+          style={{
+            width: 250,
+            height: 250,
+            resizeMode: "contain",
+          }}
+        />
+      </TouchableOpacity>
+
+      <Button
+        title="Click me (Alert.alert)"
+        color="orange"
+        onPress={() =>
+          Alert.alert("Title", "message", [
+            { text: "Yes", onPress: () => console.log("Yes tapped") },
+            { text: "No", onPress: () => console.log("No tapped") },
+          ])
+        }
+      />
+
+      {/* prompt work only in ios */}
+      <View style={{ paddingVertical: 10 }}>
+        <Button         
+          title="Click me (Alert.prompt)"
+          color="grey"
+          onPress={() =>
+            Alert.prompt("Title", "message", (string) => console.log(string))
+          }
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
